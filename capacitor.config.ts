18ad91a55e87e30b83cae5a264a729f49d034f59
Posztos Balazs <bazsiposzti@gmail.com>
2026-02-18 11:18:28 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'smart-service',
+  webDir: 'dist'
+};
+
+export default config;
